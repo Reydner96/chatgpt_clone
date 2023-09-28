87e7488ccf52e8857d7be56ca563a274174a4ca2
Reydner96 {Reyd} <31974666+Reydner96@users.noreply.github.com>
2023-09-28 11:51:54 -0300

    Update

--- server/src/config/openai.js
+++ server/src/config/openai.js
@@ -2,10 +2,10 @@ import OpenAI from "openai";
 
 module.exports = class openai{
     static configuration(){
-        const openai = new OpenAI({
+        const openai = new Configuration({
             apiKey: process.env.OPENAI_API_KEY,
           });
-          return new openai(OpenAI)
+          return new openai(configuration)
     }
     static textCompletion({prompt}) {
         return{
